cc3b21a3d09a5ffe46614e418c6b868b80278a52
Cem <cemhekimoglu.ch@gmail.com>
2018-04-16 23:01:05 +0000

    end of step 6

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,18 @@
 var request = require('request');
+var secrets = require('./secrets.js');
+// secrets.GITHUB_TOKEN
 
-console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'Authorization': 'token ' + secrets.GITHUB_TOKEN,
+    'User-Agent': 'request'
+  }
+};
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
