805a15cc223044a0b79ba3c95ca330ce7a04fbe2
Cem <cemhekimoglu.ch@gmail.com>
2018-04-17 00:59:43 +0000

    connected the functions

--- download_avatars.js
+++ download_avatars.js
@@ -15,21 +15,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function(err, res, body) {
     // cb(err, body);
-    JSON.parse(body).forEach(function(contributor) {
-      console.log(contributor.avatar_url);
-    })
+    cb(err, JSON.parse(body));
+    // JSON.parse(body).forEach(function(contributor) {
+    //   console.log(contributor.avatar_url);
+    // })
   });
 }
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-         .pipe(fs.createWriteStream(filePath));
+         .pipe(fs.createWriteStream('./avatars/' + filePath + '.png'));
 }
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
-
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
+  result.forEach(function(contributor) {
+    downloadImageByURL(contributor.avatar_url, contributor.login);
+  });
 });
